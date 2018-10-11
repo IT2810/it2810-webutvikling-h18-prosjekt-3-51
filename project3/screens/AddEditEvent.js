b99987eb7adcd39f08b7947cd255872aafc5e043
Rikard Eriksen <rikard.s.eriksen@gmail.com>
2018-10-11 14:47:11 +0200

    Events stored in AsyncStorage #1 #2

--- project3/screens/AddEditEvent.js
+++ project3/screens/AddEditEvent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button, TextInput, KeyboardAvoidingView } from "react-native";
+import { View, Text, Button, TextInput, KeyboardAvoidingView, AsyncStorage } from "react-native";
 import InputField from "../components/InputField";
 import DatePicker from "react-native-datepicker";
 import moment from "moment";
@@ -20,7 +20,8 @@ class AddEditEvent extends Component {
         startTime: moment(e.startTime),
         endTime: moment(e.endTime),
         desc: e.desc,
-        buttonEnabled: true
+        buttonEnabled: true,
+        eventExists: true
       }
     } else {
       this.state = {
@@ -29,10 +30,12 @@ class AddEditEvent extends Component {
         startTime: moment().minutes(0).seconds(0),
         endTime: moment().minutes(0).seconds(0).add(30, "minutes"),
         desc: "",
-        buttonEnabled: false
+        buttonEnabled: false,
+        eventExists: false
       }
     }
     this.checkIfValid = this.checkIfValid.bind(this);
+    this.saveEvent = this.saveEvent.bind(this);
   }
 
   checkIfValid() {
@@ -40,16 +43,65 @@ class AddEditEvent extends Component {
     return
   }
 
+  async saveEvent() {
+    try {
+      const value = await AsyncStorage.getItem("events"); // fetch all events, should come as a JSON-array
+      let allEvents = value ? JSON.parse(value) : [] // parse it to an array or create a new array in case there was none
+      allEvents.push({title: this.state.title, 
+                    date: this.state.date,
+                    startTime: this.state.startTime,
+                    endTime: this.state.endTime,
+                    desc: this.state.desc,
+                    taskDone: false})
+      await AsyncStorage.setItem("events", JSON.stringify(allEvents))
+    } catch (error) {
+      console.error("Error when saving event " + error)
+    }
+  }
+
+  async deleteCurrentEvent() {    
+    try {
+      const value = await AsyncStorage.getItem("events"); // fetch all events, should come as a JSON-array
+      if (!value) {
+        console.log("Tried deleting event but no events exist")
+        return
+      }
+      let allEvents = JSON.parse(value)
+      console.log("Pre  deletion: " + allEvents)
+      allEvents.filter(event => event.title == this.state.title && event.date == this.state.date) // naive filtering, deletes all events with the same title and date as the current state suggests
+      console.log("post deletion:" + allEvents)
+      await AsyncStorage.setItem("events", JSON.stringify(allEvents))
+    } catch (error) {
+      console.error("Error when deleting event " + error)
+    }
+  }
+
+  async debugEvents() {
+    try {
+      const value = await AsyncStorage.getItem("events"); // fetch all events, should come as a JSON-array
+      let allEvents = value ? JSON.parse(value) : [] // parse it to an array or create a new array in case there was none
+      console.log("value:")
+      console.log(value)
+      console.log("allEvents:")
+      console.log(allEvents)
+    } catch (error) {
+      console.error("Error when debugging event " + error)
+    }
+  }
+
   render() {
     const { navigate, goBack } = this.props.navigation;
     const labelStyle = { fontSize: 20, margin: 15 }
     const flexStyle = { flexDirection: "row", justifyContent: "center" }
-    const buttonStyle = { flexGrow: 1, padding: 15 }
     return (
       <KeyboardAvoidingView style={{ flex: 1 }} behavior={"padding"} enabled>
         <View style={flexStyle}>
           <Text style={labelStyle}>Title</Text>
-          <InputField placeholder={"Your title"} value={this.state.title ? this.state.title : null} multiline={true} onChangeText={(text) => { this.setState({ title: text }) }} />
+          <InputField
+            placeholder={"Your title"}
+            value={this.state.title ? this.state.title : null}
+            onChangeText={(text) => { this.setState({ title: text }) }}
+          />
         </View>
         <View style={flexStyle}>
           <Text style={labelStyle}>Date</Text>
@@ -97,8 +149,12 @@ class AddEditEvent extends Component {
         />
         <View style={{ flexDirection: "row", justifyContent: "space-around", alignItems: "center", padding: 15 }}>
           <Button
-            title="Debug"
-            onPress={() => console.warn("Result: " + JSON.stringify(this.state))}
+            title="Debug State"
+            onPress={() => console.warn("Current state: " + JSON.stringify(this.state))}
+          />
+          <Button
+            title="Debug Storage"
+            onPress={this.debugEvents}
           />
           <Button
             title="Cancel"
@@ -106,10 +162,12 @@ class AddEditEvent extends Component {
           />
           <Button
             title="Add event"
-            enabled={this.state.buttonEnabled}
-            // TODO: implement asyncstorage
-            onPress={() => alert("Result: " + JSON.stringify(this.state))}
+            onPress={() => {this.saveEvent()}}
           />
+          {this.state.eventExists && <Button
+            title="Delete event"
+            onPress={this.saveEvent}
+          />}
         </View>
 
       </KeyboardAvoidingView>
