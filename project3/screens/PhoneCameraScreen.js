9e15697bf2a43c9e9c75ecf2b32be5935a590c4a
Rikard Eriksen <rikarder@stud.ntnu.no>
2018-10-19 17:51:35 +0200

    Cleanup log statements

--- project3/screens/PhoneCameraScreen.js
+++ project3/screens/PhoneCameraScreen.js
@@ -81,7 +81,7 @@ export default class CameraExample extends React.Component {
               flexDirection: 'row',
               //alignItems: 'center', justifyContent: 'center'
               alignSelf: 'stretch',
-            }}>{console.log("test2_3")}
+            }}>
             <TouchableOpacity
               style={{
 
@@ -94,26 +94,20 @@ export default class CameraExample extends React.Component {
                     ? Camera.Constants.Type.front
                     : Camera.Constants.Type.back,
                 });
-              }}>{console.log("test2_4")}
+              }}>
               <Text
                 style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
                 {' '}Flip{' '}
-              </Text>{console.log("test2_5")}
+              </Text>
             </TouchableOpacity>
           </View>
-        </Camera>{console.log("test2_66")}
+        </Camera>
 
         <Button
-          /*onPress={() => (async () => {if (this.camera) {
-            {console.log("kjoert22")}
-            let photo = await this.camera.takePictureAsync();
-            {console.log("kjoert2")}}}
-          )()
-        }*/
+
           onPress={async () => {
             if (this.camera) {
               let photo = await this.camera.takePictureAsync();
-              { console.log("kjoert33") }
               store.save("backgroundImage", photo.uri);
               this.toggleRenderCamera(); nav("Calendar");
             }
