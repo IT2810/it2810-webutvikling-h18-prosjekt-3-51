e311226797752769644de565cd31f0f7b2d8ce95
Rikard Eriksen <rikarder@stud.ntnu.no>
2018-10-19 17:32:22 +0200

    Ability to choose local image #5

--- project3/screens/AddImageEvent.js
+++ project3/screens/AddImageEvent.js
@@ -1,62 +1,37 @@
 import React, { Component } from 'react';
 import { View, Text, Button, CameraRoll, KeyboardAvoidingView, AppRegistry, FlatList, StyleSheet, ScrollView } from "react-native";
+import { ImagePicker } from 'expo';
+import store from "react-native-simple-store";
 
 
 class AddImageEvent extends Component {
-  constructor(props) {
-  super(props);
-    this.state = {photos: [], hasPermission: false}
-  }
 
-
-  checkIfValid() {
-    return
-  }
-
-  async componentWillMount() {
-    const { status } = await Permissions.askAsync(Permissions.READ_EXTERNAL_STORAGE);
-    this.setState({ hasCameraPermission: status === 'granted' });
-  }
-
-  // _handleButtonPress = () => {
-  //    CameraRoll.getPhotos({
-  //        first: 20,
-  //        assetType: 'Photos',
-  //      })
-  //      .then((r) => {
-  //        console.log(r)
-  //       //  this.setState({ photos: r.edges });
-  //      })
-  //      .catch((err) => {
-  //         //Error Loading Images
-  //      });
-  //    };
 
   render() {
-   return (
-     <View>
-       <Button title="Load Images" onPress={async () => CameraRoll.getPhotos({
-         first: 20,
-         assetType: 'Photos',
-       }).then((photos) => {console.log(photos)})} />
-       {this.state.hasPermission ? {} : (<Text>No permission to load images!</Text>)}
-       <ScrollView>
-         {this.state.photos.map((p, i) => {
-         return (
-           <Image
-             key={i}
-             style={{
-               width: 300,
-               height: 100,
-             }}
-             source={{ uri: p.node.image.uri }}
-           />
-         );
-       })}
-       </ScrollView>
-     </View>
-   );
+    const {goBack, navigate} = this.props.navigation;
+    return (
+      <View>
+        <Button
+          title="Choose local picture"
+          onPress={async () => {await this._pickImage(); goBack(); navigate("Calendar");}}
+        />
+      </View>
+    );
   }
+
+
+  _pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    if (!result.cancelled) {
+      store.save("backgroundImage", result.uri);
+    }
+
+  };
+
 }
 
 export default AddImageEvent;
